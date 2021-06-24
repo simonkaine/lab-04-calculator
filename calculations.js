7a9dcc4e1a28d1f1a4e31afa952474bac0e79665
Simon Kaine <simonkaine@gmail.com>
2021-06-24 14:37:02 -0700

    multiply test

--- calculations.js
+++ calculations.js
@@ -5,3 +5,7 @@ export function add(num1, num2) {
 export function subtract(num1, num2) {
     return num1 - num2;
 }
+
+export function multiply(num1, num2) {
+    return num1 * num2;
+}
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
-import { subtract } from '../calculations.js';
+import { multiply } from '../calculations.js';
 
 const test = QUnit.test;
 
 
 // name your test by what it is testing
-test('test subtract', (expect) => {
+test('test multiply', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 3;
+    const expected = 10;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = (4 - 1); // use your function here
+    const actual = (5 * 3); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
