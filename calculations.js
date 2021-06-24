e6e1633fa42baac37ae177ffc79d8b87e85902fc
Simon Kaine <simonkaine@gmail.com>
2021-06-24 14:39:25 -0700

    divide test

--- calculations.js
+++ calculations.js
@@ -9,3 +9,7 @@ export function subtract(num1, num2) {
 export function multiply(num1, num2) {
     return num1 * num2;
 }
+
+export function division(num1, num2) {
+    return num1 / num2;
+}
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
-import { multiply } from '../calculations.js';
+import { division } from '../calculations.js';
 
 const test = QUnit.test;
 
 
 // name your test by what it is testing
-test('test multiply', (expect) => {
+test('test division', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 10;
+    const expected = 2.4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = (5 * 3); // use your function here
+    const actual = (5 / 2); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
