616b3f59eb80968c92ec7a37f7f829c694a16f50
Simon Kaine <simonkaine@gmail.com>
2021-06-24 13:58:08 -0700

    added the 4 basic operators

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ import { add } from "./calculations.js";
 // reference needed DOM elements
 // input 1, input 2. answer span, add button
 
+// ---------------------------------------------------- ADD 
+
 const addInput1 = document.getElementById("add-num1");
 const addInput2 = document.getElementById("add-num2");
 const addAnswerSpan = document.getElementById("add-answer");
 const addBtn = document.getElementById("add-btn");
-
 
 // set event listeners 
   // get user input(s)
@@ -23,7 +24,7 @@ const addBtn = document.getElementById("add-btn");
   // call add function with the add-num1 and add-num2
 
   addBtn.addEventListener('click', ()=> {
-    
+
     const add1 = Number(addInput1.value);
     const add2 = Number(addInput2.value);
     const answer = add(add1, add2);
@@ -32,3 +33,52 @@ const addBtn = document.getElementById("add-btn");
 
   });
 
+// ----------------------------------------------- Subtract
+
+const subInput1 = document.getElementById("sub-num1");
+const subInput2 = document.getElementById("sub-num2");
+const subAnswerSpan = document.getElementById("sub-answer");
+const subBtn = document.getElementById("sub-btn");
+
+subBtn.addEventListener("click", ()=> {
+    const sub1 = Number(subInput1.value);
+    const sub2 = Number(subInput2.value);
+    const subAnswer = (sub1 - sub2);
+    console.log(subAnswer);
+    subAnswerSpan.textContent = subAnswer;
+
+});
+
+// ----------------------------------------------- Multiply
+
+const multiplyInput1 = document.getElementById('multiply-num1');
+const multiplyInput2 = document.getElementById('multiply-num2');
+const multiplyAnswerSpan = document.getElementById('multiply-answer');
+const multiplyBtn = document.getElementById('multiply-btn');
+
+multiplyBtn.addEventListener('click', ()=> {
+    const multiply1 = (multiplyInput1.value);
+    const multiply2 = (multiplyInput2.value);
+    const multiplyAnswer = (multiply1 * multiply2);
+    console.log(multiplyAnswer);
+    multiplyAnswerSpan.textContent = multiplyAnswer;
+});
+
+// ----------------------------------------------- Divide
+
+const divideInput1 = document.getElementById('divide-num1');
+const divideInput2 = document.getElementById('divide-num2');
+const divideAnswerSpan = document.getElementById('divide-answer');
+const divideBtn = document.getElementById('divide-btn');
+
+divideBtn.addEventListener('click', ()=> {
+    const div1 = (divideInput1.value);
+    const div2 = (divideInput2.value);
+    const divAnswer = (div1 / div2);
+    console.log(divAnswer); 
+    divideAnswerSpan.textContent = divAnswer;
+});
+
+// ----------------------------------------------- Modulo
+// ----------------------------------------------- Integer Division
+// ----------------------------------------------- hypotenuse
