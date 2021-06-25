e7a37199c2ea2fb2587f2be6204a28662d9439dc
Simon Kaine <simonkaine@gmail.com>
2021-06-24 17:57:11 -0700

    added styles to calculator

--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,22 @@
 // IMPORT MODULES under test here:
-import { IntegerDivision } from '../calculations.js';
+import { hypotenuseTest } from '../calculations.js';
 
 const test = QUnit.test;
 
 
 // name your test by what it is testing
-test('test integer division', (expect) => {
+test('test hypotenuse', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 36.5;
+    const expected = 10;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    // const sum = (8**2 + 6**2); // use your function here
-    // const actual = Math.sqrt(sum);
-    const actual = Math.floor(1212 / 33)
+    const sum = (8**2 + 6**2); // use your function here
+    const actual = Math.sqrt(sum);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+    
 });
