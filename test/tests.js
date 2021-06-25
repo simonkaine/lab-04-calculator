898de7bf9c7f055a71d0e11bd5af9982c0279d95
Simon Kaine <simonkaine@gmail.com>
2021-06-24 23:45:46 -0700

    hypotenuse Test update

--- test/tests.js
+++ test/tests.js
@@ -12,8 +12,8 @@ test('test hypotenuse', (expect) => {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const sum = (8**2 + 6**2); // use your function here
-    const actual = Math.sqrt(sum);
+    const actual = hypotenuseTest(8, 6); // use your function here
+    
 
     //Expect
     // Make assertions about what is expected versus the actual result
